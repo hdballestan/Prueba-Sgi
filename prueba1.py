d405c65b8f8bec9c3c4bfda4715253fd0ffdb697
Ballesta <hdballestan@unal.edu.co>
2020-12-04 16:34:41 -0500

    Primer commit de control

---
+++ prueba1.py
@@ -0,0 +1,18 @@
+# Programa imprime los primeros n números primos
+print("Ingrese el valor de n mayor a 2\n")
+
+n= int(input())
+
+if (n<=2):
+    print("Entrada no válida")
+else:
+    for i in range(2,n+1):
+        tieneDivisores = False
+        for j in range(2,i+1):
+            if(j==i and not tieneDivisores):
+                print(i)
+            if(i%j ==0 ):
+                tieneDivisores = True
+            
+
+
